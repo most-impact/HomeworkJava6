0d4e3c498a17dc9d0aeedc162e0e12f27e6b5ffe
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2024-10-07 20:40:53 +0300

    completed tasks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,96 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Task 1
+        int totalSum = 0;
+        byte i = 0;
+        while (totalSum < 2_459_000) {
+            totalSum = (101 * (totalSum + 15000)) / 100;
+            i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSum + " рублей");
         }
+        totalSum = (101 * (totalSum + 15000)) / 100;
+        i++;
+        System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSum + " рублей");
+
+        System.out.println();
+        //Task 2
+        i = 0;
+        while (i < 10) {
+            i++;
+            System.out.print(i);
+            System.out.print(' ');
+        }
+        System.out.println();
+        while (i != 0) {
+            System.out.print(i);
+            System.out.print(' ');
+            i--;
+        }
+
+        System.out.println('\n');
+        //Task 3
+        int population = 12_000_000;
+        while (i < 10) {
+            i++;
+            population += population / 1000 * 9;
+            System.out.println("Год " + i + ", численность населения составляет " + population);
+        }
+
+        System.out.println();
+        //Task 4
+        totalSum = 15000;
+        i = 0;
+        while (totalSum < 12_000_000) {
+            totalSum += totalSum * 7 / 100;
+            i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSum + " рублей");
+        }
+
+        System.out.println();
+        //Task 5
+        totalSum = 15000;
+        i = 0;
+        while (totalSum < 12_000_000) {
+            totalSum += totalSum * 7 / 100;
+            i++;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSum + " рублей");
+            }
+        }
+
+        System.out.println();
+        //Task 6
+        totalSum = 15000;
+        i = 0;
+        while (i < 109) {
+            totalSum += totalSum * 7 / 100;
+            i++;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + totalSum + " рублей");
+            }
+        }
+
+        System.out.println();
+        //Task 7
+        int friday = 3;
+        i = 1;
+        while (i < 32) {
+            if ((i - friday) % 7 == 0) {
+                System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет");
+            }
+            i++;
+        }
+
+        System.out.println();
+        //Task 8
+        int year = 0;
+        int currentYear = 2024;
+        while (year < currentYear+100) {
+            if (year > currentYear-200) {
+                System.out.println(year);
+            }
+            year += 79;
+        }
+
     }
 }
